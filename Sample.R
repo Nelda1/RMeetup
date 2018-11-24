a3240dbe1662af8a673553e5097c596f7c50ea3b
Nelda1 <nlimilimi@gmail.com>
2018-11-24 09:34:40 +0300

    ...

--- Sample.R
+++ Sample.R
@@ -40,11 +40,34 @@ ggplot(viz, aes(x = Commercial, y = Profit, colour = "blue")) +
   geom_point(shape = 10, size = 2, show.legend = FALSE) + geom_line()
 
 #plotting a box plot
-ggplot(viz, aes(x = District, y = Profit,)) + geom_boxplot()
+ggplot(viz, aes(x = District, y = Profit,)) + geom_boxplot(outlier.colour="red", outlier.shape=8,
+                                                           outlier.size=4)
 
 #plotting piechart
 
 ggplot(data = viz, aes(x = "Profit",y= "District", fill=District)) +
   geom_bar(width = 30, stat = 'identity') +coord_polar('y',start = 20)
 
+# Commercial Distribution
+ggplot(viz, aes(x=Commercial, colour=District, fill=District)) +
+  geom_density(alpha=.3) +
+  geom_vline(aes(xintercept=mean(Commercial),  colour=District),linetype="dashed",color="grey", size=1)+
+  xlab("Ammount on Commercial") +
+  ylab("Density")+
+  ggtitle(paste("Distribution on commercial"))
 
+# Box plot
+
+ggplot(viz, aes(District, Profit, fill=District)) +
+  geom_boxplot()+
+  scale_y_continuous("Profit 'UGX' ", breaks= seq(0,1000, by=500))+
+  labs(title = "Box plot of Profits", x = "District")
+
+# Scatter
+
+ggplot(viz, aes(x = Commercial, y = Profit))+
+  xlab("Ammount")+
+  ylab("Profit") +
+  geom_point(aes(color = District,shape=District))+
+  geom_smooth(method='lm')+
+  ggtitle("Profit vs Ammount")
